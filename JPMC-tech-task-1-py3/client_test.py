4033272ff08fc8037ff0ffa2057050b005624e6f
Jui Shah <shahjui2000@gmail.com>
2020-05-02 00:41:42 +0530

    Added

---
+++ JPMC-tech-task-1-py3/client_test.py
@@ -0,0 +1,40 @@
+import unittest
+from client3 import getDataPoint, getRatio
+
+class ClientTest(unittest.TestCase):
+  def test_getDataPoint_calculatePrice(self):
+    quotes = [
+      {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+    ]
+    """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price']+quote['top_ask']['price'])/2 ) )
+
+  def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
+    quotes = [
+      {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+    ]
+    """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price']+quote['top_ask']['price'])/2 ) )
+
+
+  """ ------------ Add more unit tests ------------ """
+  def test_getRatio_bzero(self):
+    prices = [[5,0]]
+
+    for price_a, price_b in prices:
+      self.assertEqual(getRatio( price_a, price_b), None) 
+
+  def test_getRatio_azero(self):
+    prices = [[0,5]]
+
+    for price_a, price_b in prices:
+      self.assertEqual(getRatio( price_a, price_b), 0) 
+
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ README.md
@@ -0,0 +1,22 @@
+# JPMorgan Chase Software Engineering Virtual Experience
+
+Available on https://www.insidesherpa.com/ , this virtual experience has 3 main tasks which requires the skillset mentioned below in detail. They have pretty clear instructions and I would only suggest only Frontend-React beginners to attempt this. It is a very basic course, although very good for beginners.
+
+1. Interface with a stock price data feed
+   - Financial Data
+   - Python
+   - Git
+   - Basic Programming
+
+2. Use JPMorgan Chase frameworks and tools
+   - React 
+   - Typescript 
+   - Web Applications
+
+3. Display data visually for traders
+   - Technical Communication
+   - Financial Analysis
+   - Web Applications
+   
+ The duration to complete can be maximum 5 hours and upon completion, a certificate is received.
+ ![Certificate](R5iK7HMxJGBgaSbvk_JPMorgan%20Chase_ajNtQcxrgfyFafc68_completion_certificate.pdf)
